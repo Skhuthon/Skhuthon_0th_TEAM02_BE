4fce9fa93adb0787216ae90583c6d70a6321275f
Kang Duoh <kduoh99@gmail.com>
2024-06-26 16:18:19 +0900

    [Setting] 초기 세팅

---
+++ sweet_little_kitty/src/main/java/com/skhuthon/sweet_little_kitty/SweetLittleKittyApplication.java
@@ -0,0 +1,13 @@
+package com.skhuthon.sweet_little_kitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetLittleKittyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetLittleKittyApplication.class, args);
+    }
+
+}
---
+++ sweet_little_kitty/src/test/java/com/skhuthon/sweet_little_kitty/SweetLittleKittyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhuthon.sweet_little_kitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetLittleKittyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
