cb1066ea5a6133c44e2c4cdda94b84a7d760bb1f
Kang Duoh <kduoh99@gmail.com>
2024-06-27 12:02:05 +0900

    [Setting] #3 스프링 시큐리티 및 스웨거 설정 추가

---
+++ sweet_little_kitty/src/main/java/com/skhuthon/sweet_little_kitty/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.skhuthon.sweet_little_kitty.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .title("Sweet Little Kitty API")
+                .description("Sweet Little Kitty API 명세서")
+                .version("v1.0.0");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
