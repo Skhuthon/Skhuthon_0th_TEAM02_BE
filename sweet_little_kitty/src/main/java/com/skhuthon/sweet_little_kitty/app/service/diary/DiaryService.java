32bb67a678619bee66d61d599f78ef4b308debc6
Kang Duoh <kduoh99@gmail.com>
2024-06-29 22:06:32 +0900

    [Refactor] #24 JWT 토큰을 통해 인증된 사용자만 diary 작성, 삭제되도록 리팩토링

--- sweet_little_kitty/src/main/java/com/skhuthon/sweet_little_kitty/app/service/diary/DiaryService.java
+++ sweet_little_kitty/src/main/java/com/skhuthon/sweet_little_kitty/app/service/diary/DiaryService.java
@@ -3,9 +3,12 @@ package com.skhuthon.sweet_little_kitty.app.service.diary;
 import com.skhuthon.sweet_little_kitty.app.dto.diary.request.DiaryRegisterReqDto;
 import com.skhuthon.sweet_little_kitty.app.dto.diary.response.DiaryRegisterResDto;
 import com.skhuthon.sweet_little_kitty.global.template.ApiResponseTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface DiaryService {
-    ApiResponseTemplate<DiaryRegisterResDto> registerDiary(String userEmail, DiaryRegisterReqDto reqDto);
-    ApiResponseTemplate<Void> deleteDiary(String userEmail, Long diaryId);
+    ApiResponseTemplate<DiaryRegisterResDto> registerDiary(String userName, DiaryRegisterReqDto reqDto, List<MultipartFile> images);
+    ApiResponseTemplate<Void> deleteDiary(String userName, Long diaryId);
 
 }
--- sweet_little_kitty/src/main/java/com/skhuthon/sweet_little_kitty/global/security/JwtAuthenticationFilter.java
+++ sweet_little_kitty/src/main/java/com/skhuthon/sweet_little_kitty/global/security/JwtAuthenticationFilter.java
@@ -1,9 +1,11 @@
 package com.skhuthon.sweet_little_kitty.global.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -16,6 +18,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -33,13 +36,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = getTokenFromRequest(request);
         if (token != null && jwtTokenProvider.validateToken(token)) {
             String email = jwtTokenProvider.getEmailFromToken(token);
+            try {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+                if (userDetails != null) {
+                    CustomAuthenticationToken authentication = new CustomAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-            if (userDetails != null) {
-                CustomAuthenticationToken authentication = new CustomAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (UsernameNotFoundException ex) {
+                logger.error("사용자 이메일을 찾지 못했습니다 : {}");
             }
         }
 
